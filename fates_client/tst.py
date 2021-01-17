b7459710b6c2dd8444b31f75f5bc36bd8c7789a6
Rootspring <rootspring@Rootsprings-MacBook.local>
2021-01-17 19:01:55 +0530

    a

--- fates_client/tst.py
+++ fates_client/tst.py
@@ -1,6 +1,12 @@
 import asyncio
 import lib
 from discord import Client
-a = lib.FatesClient(api_token = "", client = Client())
-c = asyncio.run(a.set_guild_shard_count())
-print(c)
+client = Client()
+
+a = lib.FatesClient(api_token = "DCUaswGL6wmGskYFlVpLbIX6RcjPvnlCkzCkTPI0WiQZoqLGLjDdETA1U6gsS6tK", client = client)
+
+@client.event
+async def on_ready():
+    c = await a.set_guild_shard_count()
+    print(c)
+client.run("Nzk4OTUxNTY2NjM0Nzc4NjQx.X_8foQ.r3oWyE87FQAXx-Kf5ueyGfzDui4")
