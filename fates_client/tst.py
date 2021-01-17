30cd45d70604c135dd146bfcd7b5b84dc0a2d2ce
Rootspring <rootspring@Rootsprings-MacBook.local>
2021-01-17 23:44:47 +0530

    somthing

--- fates_client/tst.py
+++ fates_client/tst.py
@@ -7,6 +7,8 @@ a = lib.FatesClient(api_token = "DCUaswGL6wmGskYFlVpLbIX6RcjPvnlCkzCkTPI0WiQZoqL
 
 @client.event
 async def on_ready():
-    c = await a.set_guild_shard_count(len(client.guilds), client.shard_count)
+    c = await a.set_guild_shard_count(guild_count = len(client.guilds), shard_count = 0)
+    print(c)
+    c = await a.delete_event()
     print(c)
 client.run("Nzk4OTUxNTY2NjM0Nzc4NjQx.X_8foQ.r3oWyE87FQAXx-Kf5ueyGfzDui4")
