e053ecbd3a1a626b439a516be2664e926615a92a
Rootspring <rootspring@Rootsprings-MacBook.local>
2021-01-17 23:13:42 +0530

    Work on lib

--- fates_client/lib.py
+++ fates_client/lib.py
@@ -1,14 +1,22 @@
+features = []
+
 import aiohttp
 from aiohttp_requests import requests
 try:
     import fastapi
-    ws = True
+    features.append("ws")
 except:
-    ws = False
+    pass
+try:
+    from discord import Client
+    features.append("dpy")
+except:
+    Client = None
+
 import uuid
 from typing import Optional
 
-URL = "https://firestar.loca.lt" # The base fates list site
+URL = "https://rootspring.loca.lt" # The base fates list site
 
 events_api = URL + "/api/events" 
 
@@ -24,24 +32,22 @@ class AuthFailure(Exception):
     def __init__(self):
         super().__init__(f"Invalid API Token")
 
-
-class Event():
-    def __init__(self, *, id: uuid.UUID, event: str, context: Optional[str] = None):
-        self.id = id
-        self.event = event
-        if context is not None:
-            try:
-                self.context = context.split("::css=")[0]
-                self.css = context.split("::css=")[1]
-            except:
-                self.context = context
-                self.css = None
-        self.query_args = context.split("::") # These are the context arguments in a list
+class NoGSSource(Exception):
+    def __init__(self):
+        super().__init__(f"There is no source for guild/shard count. Please either pass in client to FatesClient class or pass in the guild/shard count manually")
 
 class FatesClient():
-    def __init__(self, *, api_token: str):
+    def __init__(self, *, api_token: str, client: Optional[object] = None):
         self.api_token = api_token
+        self.client = client
 
+        if self.client is None:
+            self.discord = False
+        else:
+            self.discord = True
+        
+        self.features = features
+    
     async def create_event(self, *, event: str, context: str = "NONE", css: str = None):
         if event.replace(" ", "") in ["add_bot", "edit_bot", "vote"]:
             raise InvalidEventName(event.replace(" ", ""))
@@ -60,11 +66,88 @@ class FatesClient():
             raise AuthFailure()
         return res
 
-    async def set_guild_count(self, count):
-        return await self.create_event(event = "guild_count", context = count)
+    async def delete_event(self, *, id: Optional[uuid.UUID] = None):
+        if id is None:
+            res = await requests.delete(events_api, json = {"api_token": self.api_token})
+        else:
+            res = await requests.delete(events_api, json = {"api_token": self.api_token, "event_id": str(id)})
+        try:
+            res = await res.json()        
+        except:
+            raise NetworkError()
+        if res["done"] == False and res["reason"] == "NO_AUTH":
+            raise AuthFailure()
+        return res
 
-    async def set_shard_count(self, count):
-        return await self.create_event(event = "shard_count", context = count)
+    async def enter_maint_mode(self, reason: Optional[str] = "We are currently undergoing maintenance"):
+        return await self.create_event(event = "begin_maint", context = reason)
 
-    async def set_guild_shard_count(self, guild_count, shard_count):
+    async def exit_maint_mode(self):
+        return await self.create_event(event = "exit_maint")
+
+    async def set_guild_count(self, count: Optional[int] = None):
+        if not self.discord and count is None:
+            raise NoGSSource()
+        elif count is not None:
+            return await self.create_event(event = "guild_count", context = count)
+        elif count is None:
+            return await self.create_event(event = "guild_count", context = len(client.guilds))
+
+    async def set_shard_count(self, count: Optional[int] = None):
+        if not self.discord and count is None:
+            raise NoGSSource()
+        elif count is not None:
+            return await self.create_event(event = "shard_count", context = count)
+        elif count is None:
+            return await self.create_event(event = "shard_count", context = client.shard_count)
+
+    async def set_guild_shard_count(self, guild_count: Optional[int] = None, shard_count: Optional[int] = None):
         return (await self.set_guild_count(guild_count), await self.set_shard_count(shard_count))
+
+
+class Event():
+    def __init__(self, *, fc: FatesClient, id: uuid.UUID, event: str, context: Optional[str] = None):
+        self.id = id
+        self.event = event
+        if context is not None:
+            try:
+                self.context = context.split("::css=")[0]
+                self.css = context.split("::css=")[1]
+            except:
+                self.context = context
+                self.css = None
+        else:
+            self.context = None
+            self.css = None
+        if self.context is not None:
+            self.query_args = context.split("::") # These are the context arguments in a list
+        else:
+            self.query_args = None
+        self.fc = fc
+
+    # Simple wrapper for FatesClient.delete_event
+    async def delete(self):
+       return await self.fc.delete_event(id = self.id) 
+
+    def __repr__(self):
+       return f"<APIEvent {str(self.id)}: Event={self.event}, Context={str(self.context)}, CSS={str(self.css)}, Query={str(self.query_args)}>"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __len__(self):
+        """Length of an event is how long self.event is"""
+        return len(self.event)
+
+    def __eq__(self, other):
+        """Two events are equal if their .event property is equal"""
+        return (type(other) == Event and self.id == other.id and self.event == other.event and self.context == other.context and self.css == other.css and self.query_args == other.query_args)
+
+class Vote(Event):
+    """Vote's Context is a bit special being user=UID::votes=NEW_VOTE_AMOUNT"""
+    def get_voter(self):
+        return self.context.split("::")[0].split("=")[1]
+    def get_votes(self):
+        return self.context.split("::")[1].split("=")[1]
+    def get_voter_votes(self):
+        return self.get_voter(), self.get_votes()
