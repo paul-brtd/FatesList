c5bb330b87eefb2c1fc642760dc3c71ca4f9e425
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-01-25 00:55:59 +0530

    Delete testtt.py

--- testtt.py
+++
@@ -1,1 +0,0 @@
-SET bot_id=$1, bot_library=$2, webhook=$2, description=$3, long_description=$4, prefix=$5, api_token=$6, website=$7, discord=$8, tags=$9, banner=$10, owner=$11, extra_owners=$12, invite=$13
