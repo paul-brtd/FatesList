e84794345b16669007957dd44acfb4420f8ddf47
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-01-29 14:09:19 +0530

    Update bots.py

--- modules/app/bots.py
+++ modules/app/bots.py
@@ -22,3 +22,6 @@ async def bot_desc(request: Request, bot_id: int):
     else:
         return "Bot not found! :( Try refreshing. After that either report it on the support server or just continue your day!"
 
+@router.get("/widget/{bot_id}")
+async def bot_widget(request: Request, bot_id: int):
+    bot = await db.fetchrow("SELECT prefix, shard_count, queue, description, bot_library AS library, tags, banner, website, certified, votes, servers, bot_id, invite, discord, owner, extra_owners, banner, banned, disabled, github FROM bots WHERE bot_id = $1 ORDER BY votes", bot_id)
