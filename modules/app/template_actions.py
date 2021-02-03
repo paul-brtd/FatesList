e4a31160a7f63feb9af6e3d186d67adcfeb72ca1
Meow <meow@683e51740c.servercheap.net>
2021-02-03 18:04:06 +0000

    Minor changes

--- modules/app/template_actions.py
+++ modules/app/template_actions.py
@@ -1,14 +1,14 @@
 from ..deps import *
 
 router = APIRouter(
-    prefix = "/bot",
-    tags = ["Actions"],
+    prefix = "/template",
+    tags = ["Actions (Templates)"],
     include_in_schema = False
 )
 
 @router.get("/admin/add")
 @csrf_protect
-async def add_bot(request: Request):
+async def add_template(request: Request):
     if "userid" in request.session.keys():
         form = await Form.from_formdata(request)
         if request.method == "GET":
@@ -17,7 +17,7 @@ async def add_bot(request: Request):
             for tag in TAGS:
                 new_tag = tag.replace("_", " ")
                 tags_fixed.update({tag: new_tag.capitalize()})
-            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": {"form": form}, "error": None, "mode": "add"})
+            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "templ": {"form": form}, "error": None, "mode": "templ_add"})
         else:
             owner_check = await get_user(request.session["userid"])
             if owner_check:
@@ -25,7 +25,7 @@ async def add_bot(request: Request):
             else:
                 return templates.TemplateResponse("message.html", {"request": request, "message": "Something has went wrong getting your account. Please join our support server and contact us there"})
     else:
-        request.session["RedirectResponse"] = "/bot/admin/add"
+        request.session["RedirectResponse"] = "/template/admin/add"
         return RedirectResponse("/auth/login")
 
 
@@ -59,31 +59,31 @@ async def add_bot_api(
         new_tag = tag.replace("_", " ")
         tags_fixed.update({tag: new_tag.capitalize()})
     if bot_id == "" or prefix == "" or invite == "" or description == "" or long_description == "":
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Please ensure you have filled out all the required fields.", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Please ensure you have filled out all the required fields."})
     fetch = await db.fetch("SELECT bot_id FROM bots WHERE bot_id = $1", bot_id)
     if fetch:
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "This bot already exists on Fates List", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "This bot already exists on Fates List"})
     if invite.startswith("https://discord.com") and "oauth" in invite:
         pass
     else:
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Invalid Bot Invite: Your bot invite must be in the format of https://discord.com/api/oauth2... or https://discord.com/oauth2...", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Invalid Bot Invite: Your bot invite must be in the format of https://discord.com/api/oauth2... or https://discord.com/oauth2..."})
     if len(description) > 101:
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Your short description must be shorter than 100 characters", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "Your short description must be shorter than 100 characters"})
     description = description.replace("\n", " ").replace("\t", " ")
     try:
         bot_object = await get_bot(bot_id)
     except:
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "According to Discord's API and our cache, your bot does not exist. Please try again after 2 hours.", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "According to Discord's API and our cache, your bot does not exist. Please try again after 2 hours."})
     if not bot_object:
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "According to Discord's API and our cache, your bot does not exist. Please try again after 2 hours.", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "According to Discord's API and our cache, your bot does not exist. Please try again after 2 hours."})
     if tags == "":
-        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "You must select tags for your bot", "mode": "add"})
+        return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "You must select tags for your bot"})
     selected_tags = tags.split(",")
     for test in selected_tags:
         if test in TAGS:
             pass
         else:
-            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "One of your tags doesn't exist internally. Please choose a different tags", "mode": "add"})
+            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "One of your tags doesn't exist internally. Please choose a different tags"})
     creation = time.time()
     if extra_owners == "":
         extra_owners = None
@@ -91,7 +91,7 @@ async def add_bot_api(
         try:
             extra_owners = [int(id) for id in extra_owners.split(",")]
         except:
-            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "One of your extra owners doesn't exist or you haven't comma-seperated them.", "mode": "add"})
+            return templates.TemplateResponse("add_edit.html", {"request": request, "tags_fixed": tags_fixed, "bot": bot_dict, "error": "One of your extra owners doesn't exist or you haven't comma-seperated them."})
     # Feature check + add
     features = [f for f in bot_dict.keys() if bot_dict[f] == "on" and f in ["custom_prefix", "open_source"]]
     bt.add_task(add_bot_bt, request, bot_id, prefix, library, website, banner, support, long_description, description, selected_tags, extra_owners, creation, bot_object, invite, features)
--- modules/app/templates.py
+++ modules/app/templates.py
@@ -1,21 +1,21 @@
 from ..deps import *
 
 router = APIRouter(
-    prefix = "/bot",
-    tags = ["Bots"],
+    prefix = "/template",
+    tags = ["Templates"],
     include_in_schema = False
 )
 
 @router.get("/")
-async def bot_rdir(request: Request):
+async def tmpl_rdir(request: Request):
     return RedirectResponse("/")
 
-@router.get("/{bot_id}")
-async def bot_index(request: Request, bot_id: int):
+@router.get("/{name}")
+async def tmpl_index(request: Request, bot_id: int):
     return await render_bot(request, bot_id, review = False, widget = False)
 
-@router.get("/{bot_id}/description")
-async def bot_desc(request: Request, bot_id: int):
+@router.get("/{name}/description")
+async def tmpl_desc(request: Request, bot_id: int):
     bot = await db.fetchrow("SELECT long_description FROM bots WHERE bot_id = $1",int(bot_id))
     if bot:
         return templates.TemplateResponse("description.html",{"request":request,"bot":bot})
@@ -23,5 +23,5 @@ async def bot_desc(request: Request, bot_id: int):
         return "Bot not found! :( Try refreshing. After that either report it on the support server or just continue your day!"
 
 @router.get("/{bot_id}/widget")
-async def bot_widget(request: Request, bot_id: int):
+async def tmpl_widget(request: Request, bot_id: int):
     return await render_bot(request, bot_id, review = False, widget = True)
