963a79e93b6666e1d3bf485f375de447b7322611
Meow <meow@683e51740c.servercheap.net>
2021-02-13 14:23:02 +0000

    Fix

--- modules/app/bots.py
+++ modules/app/bots.py
@@ -14,19 +14,6 @@ async def bot_rdir(request: Request):
 async def bot_index(request: Request, bot_id: int):
     return await render_bot(request, bot_id, review = False, widget = False)
 
-@router.get("/{bot_id}/description")
-async def bot_desc(request: Request, bot_id: int):
-    bot = await db.fetchrow("SELECT css, html_long_description AS html_ld, long_description FROM bots WHERE bot_id = $1",int(bot_id))
-    if bot:
-        if not bot["html_ld"]:
-            desc = markdown.markdown(bot['long_description'])
-        else:
-            desc = bot['long_description']
-        print(desc)
-        return templates.TemplateResponse("bot.html", {"request":request, "long_description": True, "bot": {"long_description": desc,  "css": bot["css"]}})
-    else:
-        return "Bot not found! :( Try refreshing. After that either report it on the support server or just continue your day!"
-
 @router.get("/{bot_id}/widget")
 async def bot_widget(request: Request, bot_id: int):
     return await render_bot(request, bot_id, review = False, widget = True)
