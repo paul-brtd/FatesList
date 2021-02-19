d568aaea89de206f1147bf2bf68f20da6015c606
Meow <meow@683e51740c.servercheap.net>
2021-02-19 05:42:39 +0000

    Better config

--- config_template.py
+++ config_template.py
@@ -1,11 +1,10 @@
-import builtins
-builtins.bot_logs=789946587203764224
-builtins.bots_role=789934898408194059
-builtins.bot_dev_role = 789935019531304991
-builtins.reviewing_server=789934742128558080 # Bit of a misnomer, but this is the actual main server
-builtins.test_server = 794834630942654546 # And THIS is the test server for reviewing bots
+bot_logs=789946587203764224
+bots_role=789934898408194059
+bot_dev_role = 789935019531304991
+reviewing_server=789934742128558080 # Bit of a misnomer, but this is the actual main server
+test_server = 794834630942654546 # And THIS is the test server for reviewing bots
 # Confusing right? Sorry, i already did 50% using reviewing server so meow ig
-builtins.staff_roles = {
+staff_roles = {
     "guild": {
         "id": 00000000000000000,
         "perm": 1
@@ -28,7 +27,7 @@ builtins.staff_roles = {
     }
 }
 # TODO: Add Timed Badges
-builtins.special_badges = {
+special_badges = {
     "STAFF": {
         "name": "Staff",
         "description": "This is a Fates List Staff Member",
@@ -45,7 +44,7 @@ builtins.special_badges = {
     }
 }
 
-builtins.features = {
+features = {
     "custom_prefix": {
         "type": "positive",
         "description": "A bot with Custom Prefix supports changing of the bot's prefix and is hence considered positive by Fates List"
@@ -56,21 +55,21 @@ builtins.features = {
     }
 } 
 
-builtins.site = "fateslist.xyz" # Replace this with your domain
+site = "fateslist.xyz" # Replace this with your domain
 
 # This value below dont need to be changed
-builtins.site_url = "https://" + site
+site_url = "https://" + site
 
-builtins.support_url = "https://discord.gg/PA5vjCRc5H"
-builtins.TOKEN = "BOT TOKEN HERE"
-builtins.TAGS = {"music": ["fas fa-music", "bg-red"], "moderation": ["fas fa-hammer", "bg-blue"], "economy": ["fa fa-viacoin", "bg-green"], "fun": ["fa fa-heart", "bg-pink"], "anime": ["fas fa-camera", "bg-red"], "games": ["fas fa-smile-o", "bg-pink"], "web_dashboard": ["fa fa-bar-chart", "bg-green"], "logging": ["fa fa-line-chart", "bg-blue"], "game_stats": ["fa fa-bar-chart", "bg-red"], "leveling": ["fa fa-long-arrow-up", "bg-green"], "roleplay": ["fas fa-cat", "bg-pink"], "utility": ["fas fa-desktop", "bg-blue"], "social": ["fa fa-users", "bg-red"]}
-builtins.pg_user = "postgres" # Postgres Database Username
-builtins.pg_pwd = "POSTGRES PASSWORD HERE" # Postgres Database Password
-builtins.csrf_secret = "CSRF_SECRET"
-builtins.session_key = "SESSION_KEY"
+support_url = "https://discord.gg/PA5vjCRc5H"
+TOKEN = ""
+TAGS = {"music": ["fas fa-music", "bg-red"], "moderation": ["fas fa-hammer", "bg-blue"], "economy": ["fa fa-viacoin", "bg-green"], "fun": ["fa fa-heart", "bg-pink"], "anime": ["fas fa-camera", "bg-red"], "games": ["fas fa-smile-o", "bg-pink"], "web_dashboard": ["fa fa-bar-chart", "bg-green"], "logging": ["fa fa-line-chart", "bg-blue"], "game_stats": ["fa fa-bar-chart", "bg-red"], "leveling": ["fa fa-long-arrow-up", "bg-green"], "roleplay": ["fas fa-cat", "bg-pink"], "utility": ["fas fa-desktop", "bg-blue"], "social": ["fa fa-users", "bg-red"]}
+pg_user = "postgres" # Postgres Database Username
+pg_pwd = "" # Postgres Database Password
+csrf_secret = ""
+session_key = ""
 class OauthConfig:
     client_id = "798951566634778641"
-    client_secret = "CLIENT SECRET"
+    client_secret = ""
     scope = ["identify"]
     redirect_uri = "https://" + site + "/auth/login/confirm"
 
