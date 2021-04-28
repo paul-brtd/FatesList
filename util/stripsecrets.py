0df400b5b4574dd121c0e1427538ee80427d9d2c
cheesycod <cheesycod@outlook.com>
2021-04-28 20:56:27 +0000

    script now also writes

--- util/stripsecrets.py
+++ util/stripsecrets.py
@@ -1,7 +1,12 @@
-"""Simple util to remove secrets from config_secrets.py"""
+"""Simple util to remove secrets from config_secrets.py. Run after making config_secrets.py to remake config_secrets_template.py"""
 secrets = open("config_secrets.py")
 contents = secrets.read()
-for line in contents.split("\n"):
+secrets.close()
+cfg = [] # Current config template in list
+for line in contents.split("\n"): # For every line, check if its proper. then try getting beginning, middle (secret) and end
     if line.replace(" ", ""):
         begin, secret, end = line.split('"')
-        print("".join((begin, '""', end)))
+        cfg.append("".join((begin, '""', end))) # Append to list
+tmpl = open("config_secrets_template.py", "w") # Open template file im write mode
+tmpl.write("\n".join(cfg)) # Write config newline seperated
+tmpl.close()
