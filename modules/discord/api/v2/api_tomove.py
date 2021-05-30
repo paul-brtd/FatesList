79e16c7a7a47ca5657b41f90c2acb964d847243a
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 16:33:21 +0530

    Update api_tomove.py

--- modules/discord/api/v2/api_tomove.py
+++ modules/discord/api/v2/api_tomove.py
@@ -636,15 +636,6 @@ async def regenerate_user_token(request: Request, user_id: int, Authorization: s
     await db.execute("UPDATE users SET api_token = $1 WHERE user_id = $2", get_token(132), id)
     return {"done": True, "reason": None, "code": 1000}
 
-@router.get("/admin/is_staff")
-async def check_staff_member(request: Request, user_id: int, min_perm: int):
-    """Admin route to check if a user is staff or not"""
-    try:
-        staff = is_staff(staff_roles, client.get_guild(main_server).get_member(user_id).roles, min_perm)
-    except:
-        return {"staff": False, "perm": 1, "sm": {}}
-    return {"staff": staff[0], "perm": staff[1], "sm": staff[2].dict()}
-
 # TODO: Paypal
 
 # Generic methods to add coins for future paypal integration
