61f01d473018e6ee6dcee60d3ef4e44d0a3ebdfb
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 16:03:56 +0530

    Update modelstomove.py

--- modules/discord/api/v2/modelstomove.py
+++ modules/discord/api/v2/modelstomove.py
@@ -13,31 +13,7 @@ import sys
 sys.path.append("modules/models") # Libraries should remove this
 import enums # as enums (for libraries)
 import datetime
-
-class BaseUser(BaseModel):
-    """
-    Represents a base user class on Fates List.
-    """
-    id: str
-    username: str
-    avatar: str
-    disc: str
-    status: enums.Status
-    bot: bool
-
-    def __str__(self):
-        """
-        :return: Returns the username
-        :rtype: str
-        """
-        return self.username
-
-    def get_status(self):
-        """
-        :return: Returns a status object for the bot
-        :rtype: Status
-        """
-        return Status(status = self.status)
+from base_models import BaseUser
 
 class BotBan(BaseModel):
     ban: bool
@@ -92,18 +68,6 @@ class BasePager(BaseModel):
 
     class Config:
         fields = {'from_': 'from'}
-
-class APIResponse(BaseModel):
-    """
-    Represents a "regular" API response on Fates List CRUD endpoints
-
-    You can check for success using the done boolean and reason using the reason attribute 
-    
-    Code is mostly random and for debugging other than 1000 and 1001 where 1000 means success and 1001 means success with message
-    """
-    done: bool
-    reason: Optional[str] = None
-    code: int = 1000
 
 class BotMaintenancePartial(BaseModel):
     type: int = 1
