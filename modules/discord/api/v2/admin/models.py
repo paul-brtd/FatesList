52fa4095d81324055a3b8e2e185b8b1cf21d9c82
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 16:10:52 +0530

    Update models.py

--- modules/discord/api/v2/admin/models.py
+++ modules/discord/api/v2/admin/models.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+import modules.models.enums as enums
 
+class BotListAdminRoute(BaseModel):
+     mod: str
+
+ class BotCertify(BotListAdminRoute):
+     certify: bool
+
+ class BotStateUpdate(BaseModel):
+     state: enums.BotState
+
+ class BotTransfer(BotListAdminRoute):
+     new_owner: str
+
+ class BotUnderReview(BotListAdminRoute):
+     requeue: enums.BotRequeue
