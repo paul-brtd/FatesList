829b7a4a7ce041b542d2f4d21bed23357a05239a
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 16:13:30 +0530

    Update models.py

--- modules/discord/api/v2/admin/models.py
+++ modules/discord/api/v2/admin/models.py
@@ -4,14 +4,30 @@ import modules.models.enums as enums
 class BotListAdminRoute(BaseModel):
      mod: str
 
- class BotCertify(BotListAdminRoute):
-     certify: bool
+class BotCertify(BotListAdminRoute):
+    certify: bool
 
- class BotStateUpdate(BaseModel):
-     state: enums.BotState
+class BotStateUpdate(BaseModel):
+    state: enums.BotState
 
- class BotTransfer(BotListAdminRoute):
-     new_owner: str
+class BotTransfer(BotListAdminRoute):
+    new_owner: str
 
- class BotUnderReview(BotListAdminRoute):
-     requeue: enums.BotRequeue
+class BotUnderReview(BotListAdminRoute):
+    requeue: enums.BotRequeue
+
+class BotQueuePatch(BotListAdminRoute):
+   feedback: Optional[str] = None 
+   approve: bool
+
+class PartialBotQueue(BaseModel):
+   user: BaseUser
+   prefix: str
+   invite: str
+   description: str
+
+class BotQueueList(BaseModel):
+   __root__: List[PartialBotQueue]
+
+class BotQueueGet(BaseModel):
+   bots: BotQueueList
