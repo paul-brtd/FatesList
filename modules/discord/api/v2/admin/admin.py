690a04189efde454ac30502b60432c25180f268f
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 16:24:46 +0530

    Update admin.py

--- modules/discord/api/v2/admin/admin.py
+++ modules/discord/api/v2/admin/admin.py
@@ -13,3 +13,11 @@ router = APIRouter(
 async def botlist_admin_console_api(request: Request):
     """API to get raw admin console info"""
     return await admin_dashboard(request) # Just directly render the admin dashboard. It knows what to do
+
+@router.patch("/bots/admin/{bot_id}/state")
+async def bot_root_update_api(request: Request, bot_id: int, data: BotStateUpdate, Authorization: str = Header("ROOT_KEY")):
+    """Root API to update a bots state. Needs the root key"""
+    if not secure_strcmp(Authorization, root_key):
+        return abort(401)
+    await db.execute("UPDATE bots SET state = $1 WHERE bot_id = $2", data.state, bot_id)
+    return {"done": True, "reason": None, "code": 1000}
