9719db172ea6c2b7337f171baae79e1e78c063c8
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 09:41:44 +0530

    Update sys.py

--- modules/core/sys.py
+++ modules/core/sys.py
@@ -1,5 +1,23 @@
 from .imports import *
 
+def setup_discord()
+    intent_main = discord.Intents.default()
+    intent_main.typing = False
+    intent_main.bans = False
+    intent_main.emojis = False
+    intent_main.integrations = False
+    intent_main.webhooks = False
+    intent_main.invites = False
+    intent_main.voice_states = False
+    intent_main.messages = False
+    intent_main.members = True
+    intent_main.presences = True
+    client = discord.Client(intents=intent_main)
+    intent_server = deepcopy(intent_main)
+    intent_server.presences = False
+    client_server = discord.Client(intents=intent_server)
+    return client, client_server
+
 # Include all the modules by looping through and using importlib to import them and then including them in fastapi
 def include_routers(fname, rootpath):
     for root, dirs, files in os.walk(rootpath):
