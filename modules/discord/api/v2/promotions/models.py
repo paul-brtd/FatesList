648ca74482fa37fc9580d8933ff7571c0a2215e3
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-03 16:16:20 +0530

    Create models.py

---
+++ modules/discord/api/v2/promotions/models.py
@@ -0,0 +1,30 @@
+class BotPromotionDelete(BaseModel):
+    """Represents a promotion delete request. Your library should internally be using this but you shouldn't need to handle this yourself """
+    id: Optional[uuid.UUID] = None
+
+class BotPromotionPartial(BaseModel):
+    """
+    Represents a partial bot promotion for creating promotions on Fates List
+    A partial promotion is similar to a regular promotion object but does not have an id
+    """
+    title: str
+    info: str
+    css: Optional[str] = None
+    type: int
+
+class BotPromotion(BotPromotionPartial):
+    """
+    Represents a bot promotion on Fates List
+    A partial promotion is similar to a regular promotion object but does not have an id
+    """
+    id: uuid.UUID
+
+#LIBRARY-INTERNAL
+class BotPromotionList(BaseModel):
+    """This is a list of bot promotions. This should be handled by your library """
+    __root__: List[BotPromotion]
+
+#LIBRARY-INTERNAL
+class BotPromotionGet(BaseModel):
+    """Represents a bot promotion response model. This should be handled by your library"""
+    promotions: BotPromotionList
