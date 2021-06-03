67c014b70bab27d305dc2236590f4c8201ecd87d
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-03 16:14:29 +0530

    Update promotions.py

--- modules/discord/api/v2/promotions/promotions.py
+++ modules/discord/api/v2/promotions/promotions.py
@@ -1,10 +1,21 @@
-@router.get("/bots/{bot_id}/promotions", response_model = BotPromotionGet, responses = {
-    404: {"model": BotPromotion_NotFound} # Promotion Not Found
+from modules.core import *
+from .models import BotPromotionPartial, BotPromotion, BotPromotions, BotPromotionPartial, BotPromotionDelete
+from modules.discord.admin import admin_dashboard
+from ..base import API_VERSION
+
+router = APIRouter(
+    prefix = f"/api/v{API_VERSION}",
+    include_in_schema = True,
+    tags = [f"API v{API_VERSION} - Promotions"]
+)
+
+@router.get("/bots/{bot_id}/promotions", response_model = BotPromotions, responses = {
+    404: {"model": APIResponse} # Promotion Not Found
 })
 async def get_promotion(request:  Request, bot_id: int):
     promos = await get_promotions(bot_id)
     if promos == []:
-        return ORJSONResponse(BotPromotion_NotFound().dict(), status_code = 404)
+        return ORJSONResponse(APIResponse(done = False, reason = "Promotion Not Found", code = "5753").dict(), status_code = 404)
     return {"promotions": promos}
 
 @router.post("/bots/{bot_id}/promotions", response_model = APIResponse, responses = {
@@ -41,7 +52,7 @@ async def edit_promotion(request: Request, bot_id: int, promo: BotPromotion, Aut
         return abort(401)
     pid = await db.fetchrow("SELECT id FROM bot_promotions WHERE id = $1 AND bot_id = $2", promo.id, bot_id)
     if pid is None:
-        return ORJSONResponse({"done":  False, "reason": "NO_PROMOTION_FOUND", "code": 2917}, status_code = 400)
+        return ORJSONResponse({"done":  False, "reason": "Promotion Not Found", "code": 2917}, status_code = 400)
     await db.execute("UPDATE bot_promotions SET title = $1, info = $2, type = $3 WHERE bot_id = $4 AND id = $5", promo.title, promo.info, promo.type, bot_id, promo.id)
     return {"done": True, "reason": None, "code": 1000}
 
@@ -58,9 +69,9 @@ async def delete_promotion(request: Request, bot_id: int, promo: BotPromotionDel
         return abort(401)
     id = id["bot_id"]
     if promo.id is not None:
-        eid = await db.fetchrow("SELECT id FROM bot_promotions WHERE id = $1", promolid)
+        eid = await db.fetchrow("SELECT id FROM bot_promotions WHERE id = $1", promo.id)
         if eid is None:
-            return ORJSONResponse({"done":  False, "reason": "NO_PROMOTION_FOUND", "code": 4848}, status_code = 400)
+            return ORJSONResponse({"done":  False, "reason": "Promotion Not Found", "code": 4848}, status_code = 400)
         await db.execute("DELETE FROM bot_promotions WHERE bot_id = $1 AND id = $2", id, promo.id)
     else:
         await db.execute("DELETE FROM bot_promotions WHERE bot_id = $1", id)
