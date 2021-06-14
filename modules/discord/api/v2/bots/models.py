6831ba182fb8aea8ef64935f6099b03d5a1542d5
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-14 12:43:13 +0530

    Update models.py

--- modules/discord/api/v2/bots/models.py
+++ modules/discord/api/v2/bots/models.py
@@ -17,3 +17,34 @@ class BotRandom(BaseModel):
     servers: str
     invite: Optional[str] = None
     votes: int
+
+class Bot(BaseUser):
+    """
+    Represents a bot on Fates List
+    """
+    description: str
+    tags: list
+    long_description_type: enums.LongDescType
+    long_description: Optional[str] = None
+    server_count: int
+    shard_count: Optional[int] = 0
+    user_count: int
+    shards: Optional[list] = []
+    prefix: str
+    library: str
+    invite: Optional[str] = None
+    invite_link: str
+    invite_amount: int
+    owners: BotOwners
+    features: list
+    state: enums.BotState
+    website: Optional[str] = None
+    support: Optional[str] = None
+    github: Optional[str] = None
+    css: Optional[str] = None
+    votes: int
+    vanity: Optional[str] = None
+    donate: Optional[str] = None
+    privacy_policy: Optional[str] = None
+    nsfw: bool
+    banner: Optional[str] = None
