6c2c8d0b942e9c508f7652a94958bc9d72534c92
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-14 12:36:12 +0530

    Update models.py

--- modules/discord/api/v2/bots/models.py
+++ modules/discord/api/v2/bots/models.py
@@ -3,3 +3,17 @@ import modules.models.enums as enums
 from ..base_models import BaseUser, APIResponse
 from typing import Optional, List
 import uuid
+
+class BotRandom(BaseModel):
+    """
+    Represents a random bot on Fates List
+    """
+    bot_id: str
+    description: str
+    banner: str
+    state: int
+    username: str
+    avatar: str
+    servers: str
+    invite: Optional[str] = None
+    votes: int
