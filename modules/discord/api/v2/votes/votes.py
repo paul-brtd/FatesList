fb6c329ed0831e86cfa127b46f1384ff02ac3941
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-15 09:29:22 +0530

    Update votes.py

--- modules/discord/api/v2/votes/votes.py
+++ modules/discord/api/v2/votes/votes.py
@@ -1,14 +1,14 @@
 from modules.core import *
-from .models import APIResponse, BotVoteCheck
+from .models import APIResponse, BotVoteCheck, BotVote
 from ..base import API_VERSION
 
 router = APIRouter(
-    prefix = f"/api/v{API_VERSION}",
+    prefix = f"/api/v{API_VERSION}/bots",
     include_in_schema = True,
     tags = [f"API v{API_VERSION} - Votes"]
 )
 
-@router.get("/bots/{bot_id}/votes", response_model = BotVoteCheck, dependencies=[Depends(RateLimiter(times=5, minutes=1))])
+@router.get("/{bot_id}/votes", response_model = BotVoteCheck, dependencies=[Depends(RateLimiter(times=5, minutes=1))])
 async def get_votes(request: Request, bot_id: int, user_id: Optional[int] = None, Authorization: str = Header("BOT_TOKEN")):
     """Endpoint to check amount of votes a user or the whole bot has."""
     if user_id is None:
@@ -23,3 +23,18 @@ async def get_votes(request: Request, bot_id: int, user_id: Optional[int] = None
     if ret is None:
         return {"votes": voter_count, "voted": voter_count != 0, "type": "VNFVote", "reason": "Voter not found!", "partial": True}
     return {"votes": voter_count, "voted": voter_count != 0, "vote_epoch": ret[0].timestamp() if isinstance(ret, tuple) else 0, "vts": voter_ts, "time_to_vote": ret[1].total_seconds() if isinstance(ret, tuple) else 0, "vote_right_now": ret == True, "type": "Vote", "reason": None, "partial": False}
+
+@router.patch("/{bot_id}/votes")
+async def create_vote(bot_id: int, data: BotVote, Authorization: str = Header("USER_TOKEN")):
+    """Endpoint to create a vote for a bot"""
+    id = await user_auth(data.user_id, Authorization)
+    if id is None:
+        return abort(401)
+
+@router.post("/{bot_id}/votes/test")
+async def send_test_webhook(bot_id: int, Authorization: str = Header("BOT_TOKEN")):
+    """Endpoint to test webhooks"""
+    id = await bot_auth(bot_id, Authorization)
+    if id is None:
+        return abort(401)
+    return await vote_bot(user_id = 519850436899897346, bot_id = bot_id, autovote = False, test = True, pretend = False)
