0cd112aa9f0bcbff3b174a8322d4222fc4422fb3
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-15 08:17:47 +0530

    Create models.py

---
+++ modules/discord/api/v2/votes/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+import modules.models.enums as enums
+from ..base_models import APIResponse
+from typing import Optional, List
+import uuid
+
+class BotVoteCheck(BaseModel):
+    votes: int
+    voted: bool
+    vote_right_now: Optional[bool] = None
+    vote_epoch: Optional[int] = None
+    time_to_vote: Optional[int] = None
+    type: str
+    reason: Optional[str] = None
