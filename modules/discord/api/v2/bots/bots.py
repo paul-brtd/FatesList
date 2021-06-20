5b0cb0227487156d0fb2881856c37baf5378ae0d
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-20 19:48:48 +0530

    Update bots.py

--- modules/discord/api/v2/bots/bots.py
+++ modules/discord/api/v2/bots/bots.py
@@ -152,3 +152,27 @@ async def set_bot_stats(request: Request, bot_id: int, api: BotStats):
             stats[stat] = stats_new[stat]
     await set_stats(bot_id = bot_id, **stats)
     return api_success()
+
+@router.post(
+    "/users/{user_id}/bots/{bot_id}", 
+    response_model = APIResponse, 
+    dependencies=[
+        Depends(RateLimiter(times=5, minutes=3)),
+        Depends(user_auth_check)
+    ]
+)
+async def add_bot(request: Request, user_id: int, bot_id: int, bot: BotMeta):
+    """
+    Adds a bot to fates list. 
+    
+    Due to how Fates List adds and edits bots using RabbitMQ, this will return a 202 and not a 200 on success
+    """
+    bot.banner = bot.banner.replace("http://", "https://").replace("(", "").replace(")", "")
+    bot_dict = bot.dict()
+    bot_dict["bot_id"] = bot_id
+    bot_dict["user_id"] = user_id
+    bot_adder = BotActions(bot_dict)
+    rc = await bot_adder.add_bot()
+    if rc is None:
+        return api_success(f"{site_url}/bot/{bot_id}", status_code = 202)
+    return api_error(rc[0])
