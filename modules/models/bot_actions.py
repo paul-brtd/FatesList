b687a46819f009947e1e6a8568416da322f031af
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-20 19:57:53 +0530

    Update bot_actions.py

--- modules/models/bot_actions.py
+++ modules/models/bot_actions.py
@@ -12,26 +12,3 @@ from pydantic import BaseModel
 import sys
 sys.path.append("modules/models") # Libraries should remove this
 import enums # as enums (for libraries)
-
-class BotMeta(BaseModel):
-    prefix: str
-    library: str
-    invite: str
-    website: Optional[str] = ""
-    description: str
-    banner: Optional[str] = ""
-    extra_owners: list
-    support: Optional[str] = ""
-    long_description: str
-    css: Optional[str] = ""
-    long_description_type: Optional[enums.LongDescType] = enums.LongDescType.html
-    nsfw: Optional[bool] = False
-    donate: Optional[str] = ""
-    privacy_policy: Optional[str] = ""
-    github: Optional[str] = ""
-    webhook_type: Optional[int] = 0
-    webhook: Optional[str] = ""
-    webhook_secret: Optional[str] = ""
-    vanity: Optional[str] = ""
-    features: Optional[list] = []
-    tags: list
