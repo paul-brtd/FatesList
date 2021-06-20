611cfa465a9861fb2340533501042bea4dcad02b
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-20 19:48:13 +0530

    Update api_tomove.py

--- modules/discord/api/v2/api_tomove.py
+++ modules/discord/api/v2/api_tomove.py
@@ -40,30 +40,6 @@ async def get_bot_ws_events(request: Request, bot_id: int):
         events = {} # Nothing
     return events
 
-@router.post(
-    "/users/{user_id}/bots/{bot_id}", 
-    response_model = APIResponse, 
-    dependencies=[
-        Depends(RateLimiter(times=5, minutes=3)),
-        Depends(user_auth_check)
-    ]
-)
-async def add_bot(request: Request, user_id: int, bot_id: int, bot: BotMeta):
-    """
-    Adds a bot to fates list. 
-    
-    Due to how Fates List adds and edits bots using RabbitMQ, this will return a 202 and not a 200 on success
-    """
-    bot.banner = bot.banner.replace("http://", "https://").replace("(", "").replace(")", "")
-    bot_dict = bot.dict()
-    bot_dict["bot_id"] = bot_id
-    bot_dict["user_id"] = user_id
-    bot_adder = BotActions(bot_dict)
-    rc = await bot_adder.add_bot()
-    if rc is None:
-        return api_success(f"{site_url}/bot/{bot_id}", status_code = 202)
-    return api_error(rc[0])
-
 @router.patch(
     "/users/{user_id}/bots/{bot_id}", 
     response_model = APIResponse, 
