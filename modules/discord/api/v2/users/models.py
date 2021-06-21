279c9b7b353d4fefb210c067be641bb7e04f6976
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-21 18:02:34 +0530

    Create models.py

---
+++ modules/discord/api/v2/users/models.py
@@ -0,0 +1,23 @@
+from modules.core import *
+class BotMeta(BaseModel):
+    prefix: str
+    library: str
+    invite: str
+    website: Optional[str] = ""
+    description: str
+    banner: Optional[str] = ""
+    extra_owners: list
+    support: Optional[str] = ""
+    long_description: str
+    css: Optional[str] = ""
+    long_description_type: Optional[enums.LongDescType] = enums.LongDescType.html
+    nsfw: Optional[bool] = False
+    donate: Optional[str] = ""
+    privacy_policy: Optional[str] = ""
+    github: Optional[str] = ""
+    webhook_type: Optional[int] = 0
+    webhook: Optional[str] = ""
+    webhook_secret: Optional[str] = ""
+    vanity: Optional[str] = ""
+    features: Optional[List[str]] = []
+    tags: List[str] 
