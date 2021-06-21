db8321feed886a423f58c6423e01d96645b89269
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-21 18:01:50 +0530

    Update models.py

--- modules/discord/api/v2/bots/models.py
+++ modules/discord/api/v2/bots/models.py
@@ -62,25 +62,4 @@ class BotStats(BaseModel):
     shards: Optional[list] = None
     user_count: Optional[int] = None
 
-class BotMeta(BaseModel):
-    prefix: str
-    library: str
-    invite: str
-    website: Optional[str] = ""
-    description: str
-    banner: Optional[str] = ""
-    extra_owners: list
-    support: Optional[str] = ""
-    long_description: str
-    css: Optional[str] = ""
-    long_description_type: Optional[enums.LongDescType] = enums.LongDescType.html
-    nsfw: Optional[bool] = False
-    donate: Optional[str] = ""
-    privacy_policy: Optional[str] = ""
-    github: Optional[str] = ""
-    webhook_type: Optional[int] = 0
-    webhook: Optional[str] = ""
-    webhook_secret: Optional[str] = ""
-    vanity: Optional[str] = ""
-    features: Optional[List[str]] = []
-    tags: List[str]              
+             
