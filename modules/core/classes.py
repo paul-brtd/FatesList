b9a728ea0accef569ed4d91819c8a2f75614488e
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 01:51:11 +0530

    Update classes.py

--- modules/core/classes.py
+++ modules/core/classes.py
@@ -4,6 +4,13 @@ from .cache import get_bot, get_user, get_any
 
 class DiscordUser(BaseModel):
     id: int
-    
+
+    # TODO: Is this the best way to do this
     async def fetch(self):
+        """Generic method to fetch a user"""
+        return await get_any(self.id)
+      
+class User(DiscordUser):
+    async def fetch(self):
+        """Fetch a user object from the discord API"""
         return await get_any(self.id)
