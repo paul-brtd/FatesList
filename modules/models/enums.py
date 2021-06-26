37d9f568e99106ee78a24fada369e86df51dc971
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 10:34:40 +0530

    Update enums.py

--- modules/models/enums.py
+++ modules/models/enums.py
@@ -6,6 +6,30 @@ from pydantic import BaseModel
 
 from config import *
 
+class BaseUser(BaseModel):
+    """
+    Represents a base user class on Fates List.
+    """
+    id: str
+    username: str
+    avatar: str
+    disc: str
+    status: enums.Status
+    bot: bool
+
+    def __str__(self):
+        """
+        :return: Returns the username
+        :rtype: str
+        """
+        return self.username
+
+    def get_status(self):
+        """
+        :return: Returns a status object for the bot
+        :rtype: Status
+        """
+        return Status(status = self.status)
 
 class TokenTypes(IntEnum):
     _init_ = "value __doc__"
