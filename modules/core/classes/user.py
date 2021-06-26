89ab9ba677026ea692aca6145d85e235ca01c045
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 11:07:59 +0530

    Update user.py

--- modules/core/classes/user.py
+++ modules/core/classes/user.py
@@ -31,37 +31,31 @@ class User(DiscordUser):
             WHERE bot_owner.owner = $1""",
             self.id
         )
-    bots = []
-    for bot in _bots:
-        bot_obj = Bot(id = bot["bot_id"])
-        bots.append(bot | {"invite": await bot_obj.invite_url(), "user": await bot_obj.fetch()})
         
-    approved_bots = [obj for obj in bots if obj["state"] in (enums.BotState.approved, enums.BotState.certified)]
-    certified_bots = [obj for obj in bots if obj["state"] == enums.BotState.certified]
+        bots = []
+        for bot in _bots:
+            bot_obj = Bot(id = bot["bot_id"])
+            bots.append(bot | {"invite": await bot_obj.invite_url(), "user": await bot_obj.fetch()})
+        
+        approved_bots = [obj for obj in bots if obj["state"] in (enums.BotState.approved, enums.BotState.certified)]
+        certified_bots = [obj for obj in bots if obj["state"] == enums.BotState.certified]
     
-    user["bot_dev"] = approved_bots != []
-    user["cert_dev"] = certified_bots != []                      
+        user["bot_dev"] = approved_bots != []
+        user["cert_dev"] = certified_bots != []                      
                          
-    guild = self.client.get_guild(main_server)
-    if guild is None:
-        badges = None
+        guild = self.client.get_guild(main_server)
+        if guild is None:
+            user["badges"] = None
                          
-    else:                      
-        user_dpy = guild.get_member(self.id)
-        if user_dpy is None:
-            user_dpy = await self.client.fetch_user(self.id)
-    
-    if user_dpy is None: # Still connecting to dpy or whatever
-        user["badges"] = None # Still not prepared to deal with it since we havent connected to discord yet 
+        else:                      
+            user_dpy = guild.get_member(self.id)            
+            user["badges"] = Badge.from_user(user_dpy, badges, user["bot_dev"], user["cert_dev"])
                          
-    else:
-        user["badges"] = Badge.from_user(user_dpy, badges, user["bot_dev"], user["cert_dev"])
-                         
-    return {
-        "bots": bots, 
-        "approved_bots": approved_bots, 
-        "certified_bots": certified_bots, 
-        "profile": user, 
-        "dup": user_dpy is None, 
-        "user": user_obj
-    }
+        return {
+            "bots": bots, 
+            "approved_bots": approved_bots, 
+            "certified_bots": certified_bots, 
+            "profile": user,
+            "dup": Fakse
+            "user": user_obj
+        }
