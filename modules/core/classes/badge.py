ddb67ecd3ebe9b3fd9042aa6a38bf0bf0df54d43
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 10:29:28 +0530

    Update badge.py

--- modules/core/classes/badge.py
+++ modules/core/classes/badge.py
@@ -10,6 +10,16 @@ from . import Bot
 
 class Badge(BaseModel):
     """Handle badges"""
+    id: str
+    name: str
+    description: str
+    image: str
+    staff: Optional[bool] = False
+    certified: Optional[bool] = False
+    bot_dev: Optional[bool] = False
+    support_server_member: Optional[bool] = False
+    everyone: Optional[bool] = False
+    
     
     @staticmethod
     def from_user(member: Member, badges: Optional[List[str]] = [], bots: List[Bot]):
@@ -28,7 +38,7 @@ class Badge(BaseModel):
             user_flags["staff"] = is_staff(staff_roles, user_dpy.roles, 2)[0]
             user_flags["support_server_member"] = True
         
-        all_badges = {}
+        all_badges = []
         for badge in badges: # Add in user created badges from blist
             try:
                 badge_data = orjson.loads(badge)
@@ -36,20 +46,16 @@ class Badge(BaseModel):
                 logger.warning("Failed to open user badge")
                 continue
                 
-            all_badges[badge_data["id"]] = {
-                "name": badge_data["name"], 
-                "description": badge_data["description"], 
-                "image": badge_data["image"]
-            }
+            all_badges.append(badge_data)
     
     # Special staff + certified badges (if present)
     for badge_id, badge_data in special_badges.items():
         for key in ("staff", "certified", "bot_dev", "support_server_member"):
             # Check if user is allowed to get badge and if so, give it
             if badge_data.get(key) and user_flags.get(key):
-                all_badges[badge_id] = badge_data
+                all_badges.append(badge_data)
             
         if badge_data.get("everyone"):
-            all_badges[badge_id] = badge_data
+            all_badges.append(badge_data)
             
     return all_badges
