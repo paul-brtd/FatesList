445211b03516858e8c38354177710210c8c8129c
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 10:13:15 +0530

    Update base.py

--- modules/core/classes/base.py
+++ modules/core/classes/base.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
 from modules.core.cache import get_any
+from asyncpg import Pool
+from discord import Client
 
-class DiscordUser(BaseModel):
-    id: int
+class DiscordUser():
+    def __init__(self, id: int, db: Pool, client: Client):
+        self.id = id
+        self.db = db
+        self.client = client
 
     # TODO: Is this the best way to do this
     async def fetch(self):
