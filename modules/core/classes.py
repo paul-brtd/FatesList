fcf79c498ad10d40f31522dfefb81480b162f901
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-26 10:08:33 +0530

    Update classes.py

--- modules/core/classes.py
+++ modules/core/classes.py
@@ -2,14 +2,6 @@
 from pydantic import BaseModel
 from .cache import get_bot, get_user, get_any
 from .badges import Badge
-
-class DiscordUser(BaseModel):
-    id: int
-
-    # TODO: Is this the best way to do this
-    async def fetch(self):
-        """Generic method to fetch a user"""
-        return await get_any(self.id)
       
 class User(DiscordUser):
     async def fetch(self):
