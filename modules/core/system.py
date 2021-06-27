a4a340295d3c46456080cc1243fcd2c3c80546a9
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-06-27 13:31:58 +0530

    Update system.py

--- modules/core/system.py
+++ modules/core/system.py
@@ -1,6 +1,11 @@
 from .imports import *
 from .ratelimits import *
 
+class FatesBot(discord.AutoShardedBot):
+    async def is_owner(self, user: discord.User):
+        if user.id == owner:
+            return True
+        return False
 
 def setup_discord():
     intent_main = discord.Intents.default()
@@ -14,17 +19,17 @@ def setup_discord():
     intent_main.messages = False
     intent_main.members = True
     intent_main.presences = True
-    client = discord.Client(intents=intent_main)
+    client = FatesBot(intents=intent_main)
     client.ready = False
     intent_server = deepcopy(intent_main)
     intent_server.presences = False
-    client_server = discord.Client(intents=intent_server)
+    client_server = FatesBot(intents=intent_server)
     return client, client_server
 
 # Include all the modules by looping through and using importlib to import them and then including them in fastapi
 def include_routers(app, fname, rootpath):
     for root, dirs, files in os.walk(rootpath):
-        if not root.startswith("_") and not root.startswith("."):
+        if not root.startswith("_") and not root.startswith(".") and not root.startswith("debug"):
             rrep = root.replace("/", ".")
             for f in files:
                 if not f.startswith("_") and not f.startswith(".") and not f.endswith("pyc") and not f.startswith("models") and not f.startswith("base"):
