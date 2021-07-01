c40de201fc0b1a7c9b0a4ddd576ac3009b1f69ef
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-01 16:24:46 +0530

    Delete utils.py

--- modules/utils.py
+++
@@ -1,4 +0,0 @@
-# Some replace tuples
-js_rem_tuple = (("onclick", ""), ("onhover", ""), ("script", ""), ("onload", ""))
-banner_replace_tuple = (("\"", ""), ("'", ""), ("http://", "https://"), ("(", ""), (")", ""), ("file://", ""))
-ldesc_replace_tuple = (("window.location", ""), ("document.ge", ""))
