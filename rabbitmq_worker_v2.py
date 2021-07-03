d797275865151477e81809d8da3d203832dfcaba
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-03 23:36:44 +0530

    Update rabbitmq_worker_v2.py

--- rabbitmq_worker_v2.py
+++ rabbitmq_worker_v2.py
@@ -1,12 +1,17 @@
-from lynxfall.rabbitmq import run
+from lynxfall.rabbit import run
 from modules.core.system import setup_discord, setup_db
 import builtins
   
 async def startup_func(logger):
     """Function that will be executed on startup"""
     state = await setup_db()
+    discord = await setup_discord()
+    state["client"] = discord[0]
     
     # For unfortunate backward compatibility
     builtins.db = state["postgres"]
     builtins.redis_db = state["redis"]
     builtins.rabbitmq_db = state["rabbit"]
+    builtins.client = state["client"]
+
+    return state
