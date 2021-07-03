7cb3a70403df03bca353d9c6cab5d6eeed6b5382
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-03 23:32:48 +0530

    Update rabbitmq_worker_v2.py

--- rabbitmq_worker_v2.py
+++ rabbitmq_worker_v2.py
@@ -5,6 +5,8 @@ import builtins
 async def startup_func(logger):
     """Function that will be executed on startup"""
     state = await setup_db()
+    
+    # For unfortunate backward compatibility
     builtins.db = state["postgres"]
     builtins.redis_db = state["redis"]
     builtins.rabbitmq_db = state["rabbit"]
