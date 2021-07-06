738e5deee89bed46b12d132f016c7d12a6022f73
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-06 15:26:16 +0530

    Update config_secrets_template.py

--- config_secrets_template.py
+++ config_secrets_template.py
@@ -4,13 +4,12 @@ csrf_secret = "" # CSRF secret
 session_key = "" # Session key
 test_server_manager_key = "" # Test server manager bot (+approve, +claim etc) key
 sentry_dsn = "" # Sentry DNS
-pg_pwd = "" # Postgres password
 ratelimit_bypass_key = "" # Key for bypassing ratelimit
 bb_key = "" # Botblock key
 test_server_manager_key = "" # Test server manager bot (+approve, +claim etc) key
-rabbitmq_pwd = "" # RabbitMQ password
 worker_key = "" # RabbitMQ worker key
 lynxfall_key = ""
 
-discord_client_id = ""
-discord_client_secret = "" # OAuth Client Secret
+discord_client_id = "" # Discord client id
+discord_client_secret = "" # Discord client secret
+discord_redirect_uri = "" # Discord redirect uri
