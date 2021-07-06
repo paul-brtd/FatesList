e1caae3bea673dc4d36fee79f3083154149bd969
youexample.com <you@examplse.com>
2021-07-06 11:00:41 +0000

    Recreate template for secrets

--- config_secrets_template.py
+++ config_secrets_template.py
@@ -1,15 +1,11 @@
 TOKEN_MAIN = "" # Main bot token
-TOKEN_SERVER = "" # Main bot token
-csrf_secret = "" # CSRF secret
+TOKEN_SERVER = "" # Servers bot token
 session_key = "" # Session key
 test_server_manager_key = "" # Test server manager bot (+approve, +claim etc) key
-sentry_dsn = "" # Sentry DNS
+sentry_dsn = "" # Sentry DSN
 ratelimit_bypass_key = "" # Key for bypassing ratelimit
-bb_key = "" # Botblock key
-test_server_manager_key = "" # Test server manager bot (+approve, +claim etc) key
 worker_key = "" # RabbitMQ worker key
-lynxfall_key = ""
-
+lynxfall_key = "" # Lynxfall Oauth key
 discord_client_id = "" # Discord client id
 discord_client_secret = "" # Discord client secret
 discord_redirect_uri = "" # Discord redirect uri
