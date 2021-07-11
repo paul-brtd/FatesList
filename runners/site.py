a2764b98aa3ea62a5e27ddd5b82fa83bbffa74ca
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-11 20:49:06 +0530

    Update site.py

--- runners/site.py
+++ runners/site.py
@@ -5,13 +5,14 @@ uvloop.install()
 from fastapi import FastAPI
 from modules.core.system import init_fates_worker
 from fastapi.responses import ORJSONResponse
+from config import API_VERSION
 
 # Setup FastAPI with required urls and orjson for faster json handling
 app = FastAPI(
     default_response_class = ORJSONResponse, 
-    redoc_url = "/api/docs/redoc", 
-    docs_url = "/api/docs/swagger", 
-    openapi_url = "/api/docs/openapi"
+    redoc_url = f"/api/v{API_VERSION}/docs/redoc", 
+    docs_url = f"/api/v{API_VERSION}/docs/swagger", 
+    openapi_url = f"/api/v{API_VERSION}/docs/openapi"
 )
 
 @app.on_event("startup")
