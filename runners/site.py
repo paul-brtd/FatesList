c9e8c11739291ba5f3ac048ef0ea62a18f6167df
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-11 15:47:13 +0530

    Update site.py

--- runners/site.py
+++ runners/site.py
@@ -2,14 +2,9 @@ import uvloop
 
 uvloop.install()
 
-from fastapi import FastAPI, Request
-from fastapi.exceptions import (
-    HTTPException, RequestValidationError, ValidationError
-)
-from loguru import logger
-from fastapi.responses import ORJSONResponse
+from fastapi import FastAPI
 from modules.core.system import init_fates_worker
-from modules.core.error import WebError
+from fastapi.responses import ORJSONResponse
 
 # Setup FastAPI with required urls and orjson for faster json handling
 app = FastAPI(
@@ -19,17 +14,6 @@ app = FastAPI(
     openapi_url = "/api/docs/openapi"
 )
 
-# Setup exception handling
-@app.exception_handler(403)
-@app.exception_handler(404)
-@app.exception_handler(RequestValidationError)
-@app.exception_handler(ValidationError)
-@app.exception_handler(500)
-@app.exception_handler(HTTPException)
-@app.exception_handler(Exception)
-async def fl_exception_handler(request, exc, log = True):
-    return await WebError.error_handler(request, exc, log = log)
-
 @app.on_event("startup")
 async def startup():
-    await init_fates_worker(app, fl_exception_handler)
+    await init_fates_worker(app)
