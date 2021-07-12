05f41940f46e09e0a59c45cbe5a4caf8a7516dbe
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-12 16:45:04 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -18,22 +18,25 @@ app.add_typer(rabbit, name="rabbit")
 
 @site.command("run")
 def run_site(
-    workers: int = typer.Argument(3, envvar="SITE_WORKERS"),
-    session_id: uuid.UUID = typer.Argument(uuid.uuid4(), envvar="SESSION_ID")
+    workers: int = typer.Argument(3, envvar="SITE_WORKERS")
 ):
-
-    # Setup FastAPI with required urls and orjson for faster json handling
-    app = FastAPI(
+    session_id = uuid.uuid4()
+    os.system(f"python3.10 -m gunicorn 'manage:_appgen()'")
+    
+def _appgen(workers, session_id):
+    """Make the FastAPI app for gunicorn"""
+    _app = FastAPI(
         default_response_class = ORJSONResponse, 
         redoc_url = f"/api/v{API_VERSION}/docs/redoc", 
         docs_url = f"/api/v{API_VERSION}/docs/swagger", 
         openapi_url = f"/api/v{API_VERSION}/docs/openapi"
     )
 
-    @app.on_event("startup")
+    @_app.on_event("startup")
     async def startup():
-        await init_fates_worker(app, workers, session_id)
+        await init_fates_worker(_app, workers, session_id)
     
+    return _app
 
 if __name__ == "__main__":
     app()
