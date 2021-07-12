b796cc3c626552481c374066e4bc4d183c17b88b
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-12 16:31:54 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,6 +1,13 @@
+import uvloop
+uvloop.install()
+from modules.core.system import init_fates_worker
+from fastapi.responses import ORJSONResponse
+from config import API_VERSION
+
 import typer
 import importlib
-
+import uuid
+from fastapi import FastAPI
 
 app = typer.Typer()
 site = typer.Typer()
@@ -10,10 +17,23 @@ app.add_typer(rabbit, name="rabbit")
 
 
 @site.command("run")
-def run_site():
-    app = FastAPI
-    
+def run_site(
+    workers: int = typer.Argument(3, envvar="SITE_WORKERS"),
+    session_id: uuid.UUID = typer.Argument(uuid.uuid4(), envvar="SESSION_ID")
+):
 
+    # Setup FastAPI with required urls and orjson for faster json handling
+    app = FastAPI(
+        default_response_class = ORJSONResponse, 
+        redoc_url = f"/api/v{API_VERSION}/docs/redoc", 
+        docs_url = f"/api/v{API_VERSION}/docs/swagger", 
+        openapi_url = f"/api/v{API_VERSION}/docs/openapi"
+    )
+
+    @app.on_event("startup")
+    async def startup():
+        await init_fates_worker(app, workers, session_id)
+    
 
 if __name__ == "__main__":
     app()
