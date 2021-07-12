2d5ced6085afdaafb8b396278fdbccf51cc82acf
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-12 18:56:41 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import typer
 import importlib
 import uuid
+import asyncio
+import uvloop
+uvloop.install()
 
 app = typer.Typer()
 site = typer.Typer()
@@ -13,8 +16,6 @@ app.add_typer(rabbit, name="rabbit")
 
 def _fappgen():
     """Make the FastAPI app for gunicorn"""   
-    import uvloop
-    uvloop.install()
     from modules.core.system import init_fates_worker
     from fastapi.responses import ORJSONResponse
     from config import API_VERSION
@@ -49,6 +50,45 @@ def run_site(
         }
     )
 
+@rabbit.command("run")
+def run_rabbit():
+    from lynxfall.rabbit.launcher import run
+    from modules.core.system import setup_discord, setup_db
+    from config import TOKEN_MAIN
+    from config import worker_key
+  
+    async def on_startup(state, logger):
+        """Function that will be executed on startup"""
+        dbs = await setup_db()
+        state.__dict__.update(dbs)
+        discord = await setup_discord()
+        state.client = discord["main"]
+        
+        # For unfortunate backward compatibility with functions that havent ported yet
+        builtins.db = state.postgres
+        builtins.redis_db = state.redis
+        builtins.rabbitmq_db = state.rabbit
+        builtins.client = builtins.dclient = state.client
 
+    async def on_prepare(state, logger):
+        """Function that will prepare our worker"""
+        return await state.client.wait_until_ready()
+
+    async def on_stop(state, logger):
+        """Function that will run on stop"""
+
+    async def on_error(state, logger, message, exc, exc_type, exc_context):
+        pass
+
+    run(
+        worker_key = worker_key, 
+        backend_folder = "modules/rabbitmq", 
+        on_startup = on_startup, 
+        on_prepare = on_prepare,
+        on_stop = on_stop, 
+        on_error = on_error
+    )  
+    
+    
 if __name__ == "__main__":
     app()
