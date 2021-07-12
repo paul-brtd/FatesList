b304a5721c13c5ed969319ab21c3ebf1c9c8ff10
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-12 19:26:31 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -40,7 +40,7 @@ def run_site(
     workers: int = typer.Argument(3, envvar="SITE_WORKERS")
 ):
     session_id = uuid.uuid4()
-    os.execvp("gunicorn", [
+    os.execvpe("gunicorn", [
         "--log-level=debug", 
         "-p", "/home/meow/flmain.pid",
         "-k", "config._uvicorn.FatesWorker",
