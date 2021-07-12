08cbc0287b57cff37d501d978c3c0ba539d0e4fe
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-12 18:52:01 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,41 +11,44 @@ rabbit = typer.Typer()
 app.add_typer(rabbit, name="rabbit")
 
 
-@site.command("run")
-def run_site(
-    workers: int = typer.Argument(3, envvar="SITE_WORKERS")
-):
-    session_id = uuid.uuid4()
-    subprocess.Popen(f"python3.10 -m gunicorn --log-level=debug -p ~/flmain.pid -k config._uvicorn.FatesWorker 'manage:_fappgen()' -b 0.0.0.0:9999 -w {workers}", shell = True, 
-    env = os.environ | {
-        "LOGURU_LEVEL": "DEBUG",
-        "SESSION_ID": str(session_id),
-        "WORKERS": str(workers)
-    }).wait()
-    
 def _fappgen():
-    """Make the FastAPI app for gunicorn"""
-    
+    """Make the FastAPI app for gunicorn"""   
     import uvloop
     uvloop.install()
     from modules.core.system import init_fates_worker
     from fastapi.responses import ORJSONResponse
     from config import API_VERSION
     from fastapi import FastAPI 
-    
-   
-    _app = FastAPI(
+     
+    site = FastAPI(
         default_response_class = ORJSONResponse, 
-        redoc_url = f"/api/v{API_VERSION}/docs/redoc", 
-        docs_url = f"/api/v{API_VERSION}/docs/swagger", 
+        redoc_url = f"/api/v{API_VERSION}/docs/redoc",
+        docs_url = f"/api/v{API_VERSION}/docs/swagger",
         openapi_url = f"/api/v{API_VERSION}/docs/openapi"
     )
 
-    @_app.on_event("startup")
+    @site.on_event("startup")
     async def startup():
-        await init_fates_worker(_app)
+        await init_fates_worker(site)
     
-    return _app
+    return site
+
+
+@site.command("run")
+def run_site(
+    workers: int = typer.Argument(3, envvar="SITE_WORKERS")
+):
+    session_id = uuid.uuid4()
+    subprocess.Popen(
+        f"python3.10 -m gunicorn --log-level=debug -p ~/flmain.pid -k config._uvicorn.FatesWorker 'manage:_fappgen()' -b 0.0.0.0:9999 -w {workers}", 
+        shell = True, 
+        env = os.environ | {
+            "LOGURU_LEVEL": "DEBUG",
+            "SESSION_ID": str(session_id),
+            "WORKERS": str(workers)
+        }
+    )
+
 
 if __name__ == "__main__":
     app()
