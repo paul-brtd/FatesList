18016c3c4e1e06b8397032a5ab4bdad1d53a561a
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-23 13:34:55 +0530

    Update models.py

--- modules/discord/api/v2/reviews/models.py
+++ modules/discord/api/v2/reviews/models.py
@@ -1,13 +1,14 @@
 import uuid
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 import modules.models.enums as enums
 
 from ..base_models import APIResponse, BaseUser
 
 class BotReviewPartial(BaseModel):
+    id: Optional[uuid.UUID] = None
     review: str
     star_rating: float
     reply: bool
