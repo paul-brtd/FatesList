8aacee6df23ba561d904b14d760647f534607391
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-07-23 13:14:26 +0530

    Update models.py

--- modules/discord/api/v2/reviews/models.py
+++ modules/discord/api/v2/reviews/models.py
@@ -1,1 +1,12 @@
+import uuid
+from typing import List, Optional
 
+from pydantic import BaseModel
+
+import modules.models.enums as enums
+
+from ..base_models import APIResponse, BaseUser
+
+class BotReviewPartial(BaseModel):
+    review: str
+    star_rating: float
