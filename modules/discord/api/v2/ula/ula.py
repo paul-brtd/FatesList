1826b31cac0456dc3f449bad496356eacf6b3081
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-08-03 15:31:19 +0530

    Update ula.py

--- modules/discord/api/v2/ula/ula.py
+++ modules/discord/api/v2/ula/ula.py
@@ -13,28 +13,6 @@ class Supported:
     get_user_voted = ('user_id', 'res_voted') # Get User Votes
 # Base Models
 
-class Lists(BaseModel):
-    lists: dict
-
-class Stats(Lists):
-    server_count: int
-    shard_count: int
-
-class BList(BaseModel):
-    url: str
-    icon: Optional[str] = None
-    api_url: str
-    api_docs: str
-    discord: Optional[str] = None
-    description: Optional[str] = "No Description Yet :("
-    supported_features: List[int]
-    owners: List[str]
-
-class Endpoint(BaseModel):
-    method: enums.ULAMethod
-    feature: enums.ULAFeature
-    api_path: str
-    supported_fields: dict
 
 @router.get("/list/{url}")
 async def get_list(request: Request, url: str):
