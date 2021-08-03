90e3a28324875e23744fe312e391aeb9911cd606
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-08-03 15:50:09 +0530

    Update schema.sql

--- data/snowfall/sql/schema.sql
+++ data/snowfall/sql/schema.sql
@@ -226,6 +226,8 @@ CREATE TABLE server_tags (
     CONSTRAINT guilds_fk FOREIGN KEY (guild_id) REFERENCES servers(guild_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
+
+-- ULA
 CREATE TABLE bot_list_feature (
 	feature_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL UNIQUE,
@@ -249,4 +251,39 @@ CREATE TABLE bot_list_partners (
 	created_at timestamptz default now(),
 	js_allowed boolean default true,
 	published boolean default false
+);
+
+CREATE TABLE bot_list (
+	icon TEXT,
+	url TEXT NOT NULL UNIQUE,
+	api_url TEXT,
+	api_docs TEXT,
+	discord TEXT,
+	description TEXT,
+	supported_features INTEGER[],
+	state INTEGER DEFAULT 0,
+	owners BIGINT[] DEFAULT '{}'
+);
+
+CREATE TABLE bot_list_feature (
+	feature_id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE,
+	iname TEXT NOT NULL UNIQUE, -- Internal Name
+	description TEXT,
+	positive INTEGER
+);
+
+CREATE TABLE bot_list_api (
+	id SERIAL PRIMARY KEY, -- Django'isms and good for us
+	url TEXT NOT NULL,
+	method INTEGER, -- 1 = GET, 2 = POST, 3 = PATCH, 4 = PUT, 5 = DELETE
+	feature INTEGER, -- 1 = Get Bot, 2 = Post Stats
+	supported_fields JSONB, -- Supported fields
+	api_path TEXT NOT NULL,
+	CONSTRAINT url_constraint FOREIGN KEY (url) REFERENCES bot_list(url) ON DELETE CASCADE ON UPDATE CASCADE -- Autoupdate
+);
+
+CREATE TABLE ula_user (
+	user_id BIGINT NOT NULL,
+	api_token TEXT NOT NULL UNIQUE
 );
