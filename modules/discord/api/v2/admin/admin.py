6aefe107fa178c56d9824881085682bf83e6805e
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-08-05 14:42:16 +0530

    Update admin.py

--- modules/discord/api/v2/admin/admin.py
+++ modules/discord/api/v2/admin/admin.py
@@ -63,7 +63,6 @@ async def bot_admin_operation(request: Request, bot_id: int, data: BotAdminOpEnd
                 status_code = 429, 
                 headers = {"X-OP-RL": "1", "Retry-After": str(coolkey)}
             )
-        await redis_db.set(f"cooldown-{data.op.__cooldown__.name}-{user.id}", 0, px = int(data.op.__cooldown__.value*1000))
 
     # Check that reason is given where needed
     if data.op.__reason_needed__ and not data.reason:
@@ -300,7 +299,8 @@ async def bot_admin_operation(request: Request, bot_id: int, data: BotAdminOpEnd
                 return api_error(rc, 2760)
         else:
             asyncio.create_task(tool)
-            
+    
+    await redis_db.set(f"cooldown-{data.op.__cooldown__.name}-{user.id}", 0, px = int(data.op.__cooldown__.value*1000))
     return api_success(success_msg, status_code = success_code)
 
 @router.get("/queue/bots", response_model = BotQueueGet)
