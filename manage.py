a6e12dcefbb8aa86f385a6d4cfdfce9e23a84e66
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-08-13 23:50:41 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -416,7 +416,7 @@ def db_apply(module):
 
 
 @db.command("wipeuser")
-@click.argument('user_id', type=click.types.INT, required=True, help="Input user id to wipe")
+@click.option('--user', "user_id", type=click.types.INT, required=True, help="Input user id to wipe")
 def db_wipeuser(user_id):
     """Wipes a user account (e.g. Data Deletion Request)"""
     import uvloop
