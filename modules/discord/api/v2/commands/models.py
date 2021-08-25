a3466cd51627774da5b1b0b6bac1fe0f662d2b45
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-08-25 10:04:24 +0530

    Create models.py

---
+++ modules/discord/api/v2/commands/models.py
@@ -0,0 +1,17 @@
+class BotCommand(BaseModel):
+    cmd_type: enums.CommandType # 0 = no, 1 = guild, 2 = global
+    cmd_groups: Optional[List[str]] = ["Default"]
+    cmd_name: str
+    vote_locked: bool
+    description: str
+    args: Optional[list] = ["<user>"]
+    examples: Optional[list] = []
+    premium_only: Optional[bool] = False
+    notes: Optional[list] = []
+    doc_link: Optional[str] = ""
+
+class BotCommandWithId(BotCommand):
+    id: uuid.UUID
+
+class BotCommandsGet(BaseModel):
+    __root__: Dict[str, List[BotCommandWithId]]
