1da24d4aa76c7f7186e6f04e867819a00c06e335
cheesycod <cheesycod@outlook.com>
2021-09-30 09:06:15 +0000

    almost ready for ws release

--- data/snowfall/info/ws_example.py
+++ data/snowfall/info/ws_example.py
@@ -11,12 +11,21 @@ import websockets
 
 from modules.models import enums
 
+async def render_events(event):
+    for m in event.split("\x00"):
+        print(json.loads(m))
+
+
 # Not yet done
 async def run():
     async with websockets.connect("ws://localhost:10293") as websocket:
         while True:
             event = await websocket.recv()
-            event = json.loads(event)
+            try:
+                event = json.loads(event)
+            except:
+                asyncio.create_task(render_events(event))
+                event = {}
             print(f"DEBUG: Got {event}")
             if event.get("code") == "identity":
                 print("Got IDENTITY payload")
@@ -30,12 +39,11 @@ async def run():
                     api_token = "55gCmZ7zr12upTnQcvnrXcJv1IfN15ddk9WLlxG0h54uCGKFi2TBPlOFh8RYhbCMaSDQPCju2k0g2pykEmsD3AmEvUNPoc4Rxqjk6fpNqncjk8PVeh2ImolpaXE1cNEdCVEh"
                 else:
                     api_token = input("Enter API Token: ")
-                payload = {"id": str(bot_id), "token": api_token, "bot": True}
+                payload = {"id": str(bot_id), "token": api_token, "bot": True, "send_all": False, "send_none": True}
                 await websocket.send(json.dumps(payload))
                 print(f"Sending {json.dumps(payload)}")
-                res = await websocket.recv()
-                print(f"DEBUG: Got response {res}")
-                event = json.loads(res)
+            res = await websocket.recv()
+            print(f"DEBUG: Got response {res}")
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 loop.run_until_complete(run())
