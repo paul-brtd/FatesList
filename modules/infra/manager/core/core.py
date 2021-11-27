00835832c13af75547990ad4816803deb6919d23
cheesycod <cheesycod@outlook.com>
2021-11-27 13:48:37 +0000

    clean up core

--- modules/infra/manager/core/core.py
+++ modules/infra/manager/core/core.py
@@ -30,13 +30,11 @@ class InteractionWrapper():
     async def auto_defer(self, ephemeral: bool):
         start_time = time.time()
         while time.time() - start_time < 15 and not self.interaction.response.is_done():
-            await asyncio.sleep()
+            await asyncio.sleep(0)
         await self.interaction.defer(ephemeral = ephemeral)
     
     async def send(self, *args, **kwargs):
-        if self.interaction.response.is_done():
-            return await self.interaction.followup.send(*args, **kwargs)
-        return await self.interaction.response.send_message(*args, **kwargs)
+        return await self.interaction.send(*args, **kwargs)
 
 class MenuState(IntEnum):
     rot = 0 # Running or timed out
@@ -61,52 +59,6 @@ class BotListView(discord.ui.View):
            
         self.select_menu.add_option(label="Not listed", value="-1")
         self.add_item(self.select_menu)   
-    
-class MenuView(discord.ui.View):
-    def __init__(self, embeds):
-        super().__init__()
-        self.embeds = embeds
-        self.current = 0
-        self.state = MenuState.rot
-    
-    async def respond(self, interaction):
-        await interaction.response.edit_message(embed=self.embeds[self.current])
-    
-    @discord.ui.button(label='Back', emoji = '⏮️')
-    async def back(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # Back should go back or go to last page if going back would lead to negative index
-        if self.current > 0:
-            self.current -= 1
-        elif len(self.embeds) > 0:
-            self.current = len(self.embeds) - 1
-        elif len(self.embeds) > 1:
-            self.current = len(self.embeds) - 2
-           
-        await self.respond(interaction)
-    
-    @discord.ui.button(label='Cancel', emoji='❌')
-    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
-        self.state = MenuState.cancelled
-        self.stop()
-    
-    @discord.ui.button(label='Next', emoji='⏭️')
-    async def next(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # Next should go next and go to first if going next would go past the embed list
-        self.current = self.current + 1 if self.current + 1 < len(self.embeds) else 0
-        await self.respond(interaction)
-        
-        
-# Simple button menu, remove when redbot adds proper support
-async def menu(interaction, embeds):
-    view = MenuView(embeds)
-    await interaction.response.send_message(embed=embeds[0], view=view, ephemeral=True)
-    await view.wait()
-    if view.state == MenuState.cancelled:
-        s = "Cancelled"
-    else:
-        s = "Timed out!"
-    await interaction.edit_original_message(content=s, embed=None, view=None)
-    
     
 async def request(method: str, ctx, url: str, dragon_auth: bool = True, user_token: Optional[str] = None, **kwargs):
     url = f"http://127.0.0.1:9999{url}"
@@ -148,30 +100,6 @@ async def log(ctx, message):
     channel = ctx.bot.get_channel(log_channel)
     await channel.send(message)                 
 
-async def get_bot_info(ctx, bot_dat):
-    if isinstance(bot_dat, discord.User):
-        return bot_dat.name, bot_dat.id, bot_dat.discriminator, {}
-    bot = await request("GET", ctx, f"/api/bots/{bot_dat}", staff = False)
-    if isinstance(bot_dat, int):
-        if bot[0] != 200:
-            name = "Unknown User"
-            disc = "0000"
-            bot_id = "000000000000000000"
-        elif bot_dat == 0:
-            name = "Recursive Command"
-            disc = "0000"
-            bot_id = "000000000000000000"
-        else:
-            name = bot[1]["user"]["username"]
-            disc = bot[1]["user"]["disc"]
-            bot_id = bot[1]["user"]["id"]
-    else:
-        name = "Unknown User"
-        disc = "0000"
-        bot_id = "000000000000000000"
-    return name, bot_id, disc, bot[1]
-
-
 async def profile(ctx, user):
     """Gets the users profile, sends a message and returns None if not found"""
     if user.bot:
@@ -182,7 +110,7 @@ async def profile(ctx, user):
     return res[1]
  
 # usage: (_d, _h, _m, _s, _mils, _mics) = tdTuple(td)
-def __tdTuple(td:datetime.timedelta) -> tuple:
+def extract_time(td:datetime.timedelta) -> tuple:
     def _t(t, n):
         if t < n: return (t, 0)
         v = t//n
@@ -202,9 +130,9 @@ async def blstats(ctx):
         return await blstats(ctx)
 
     embed = Embed(title = "Bot List Stats", description = "Fates List Stats")
-    uptime_tuple = __tdTuple(datetime.timedelta(seconds = res[1]['uptime']))
+    uptime_tuple = extract_time(datetime.timedelta(seconds = res[1]['uptime']))
     # ttvr = Time Till Votes Reset
-    ttvr_tuple = __tdTuple((datetime.datetime.now().replace(day=1, second = 0, minute = 0, hour = 0) + datetime.timedelta(days=32)).replace(day=1) - datetime.datetime.now())
+    ttvr_tuple = extract_time((datetime.datetime.now().replace(day=1, second = 0, minute = 0, hour = 0) + datetime.timedelta(days=32)).replace(day=1) - datetime.datetime.now())
     uptime = "{} days, {} hours, {} minutes, {} seconds".format(*uptime_tuple)
     ttvr = "{} days, {} hours, {} minutes, {} seconds".format(*ttvr_tuple)
     embed.add_field(name = "Uptime", value = uptime)
