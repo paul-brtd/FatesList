1d828de5678aefde1ef842b5ea9b0efafba1c1fe
Paul <peberthodin@outlook.com>
2021-12-05 12:22:03 +0100

    start fixing a few typos and rephrase etc

--- data/static/assets/src/vote.js
+++ data/static/assets/src/vote.js
@@ -1,20 +1,20 @@
 function voteBot() {
 	if(context.type == "server") {
-		modalShow("Voting for servers", "Do /vote in this server to cote for it and get exclusive perks if the server supports vote rewards")
+		modalShow("Voting for servers", "Do /vote in this server to vote for it and get exclusive perks if the server supports vote rewards")
 		return
 	}
 	if(!context.logged_in) {
 		window.location.href = `/fates/login?redirect=/${context.type}/${context.id}/vote&pretty=to vote for this bot`
 		return
 	}
-	modalShow("Voting...", "Please wait...")
+	modalShow("Sending your vote", "Please wait...")
 	request({
 		url: `/api/dragon/users/vote?user_id=${context.user_id}&bot_id=${context.id}&test=false`,
 		method: "POST",
 		userAuth: true,
 		statusCode: {
 			200: function(data) {
-				modalShow("Voted!", "You have successfully voted for this bot")
+				modalShow("Vote Succesful!", "You have successfully voted for this bot")
 				setTimeout(() => (window.location.href = `/${context.type}/${context.id}`), 1500)
 			}
 		}
